a958c9657b92eda190f449e7fe6de77a1e3345cf
amurray101 <amurray101@gmail.com>
2017-07-09 12:38:48 -0700

    debugging views -- mystery post.objects.get(pk=pk) error

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
-from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 from django.shortcuts import redirect
 
 # Create your views here.
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {})
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
-Post.objects.get(pk=pk)
+#Post.objects.get(pk=pk)
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
